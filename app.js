7baccd0c3879fdd5ebfef1440e996bd5dd6c610b
sammy <sammykkoon@gmail.com>
2021-09-24 19:59:43 +0900

    jwt 토큰 학습 진행

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 const passport = require("passport");
+const v1 = require("./routes/v1");
 
 dotenv.config();
 const pageRouter = require("./routes/page");
@@ -20,18 +21,18 @@ passportConfig();
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "html");
 nunjucks.configure("views", {
-  express: app,
-  watch: true,
+    express: app,
+    watch: true,
 });
 
 sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log("database conntection success!");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+    .sync({ force: false })
+    .then(() => {
+        console.log("database conntection success!");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -40,37 +41,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  })
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+    }),
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use("/v1", v1);
 app.use("/", pageRouter);
 app.use("/auth", authRouter);
 app.use("/post", postRouter);
 app.use("/user", userRouter);
 
 app.use((req, res, next) => {
-  const error = new Error(`${req.method} ${req.url} Router does not exist!`);
-  error.status = 404;
-  next(error);
+    const error = new Error(`${req.method} ${req.url} Router does not exist!`);
+    error.status = 404;
+    next(error);
 });
 
 app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트 대기중");
+    console.log(app.get("port"), "번 포트 대기중");
 });
--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,16 +1,36 @@
+const jwt = require("jsonwebtoken");
+
 exports.isLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(403).send(" require login!");
-  }
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.status(403).send(" require login!");
+    }
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    next();
-  } else {
-    const message = encodeURIComponent("already LoggedIn");
-    res.redirect(`/?error=${message}`);
-  }
+    if (!req.isAuthenticated()) {
+        next();
+    } else {
+        const message = encodeURIComponent("already LoggedIn");
+        res.redirect(`/?error=${message}`);
+    }
+};
+
+exports.verifyToken = (req, res, next) => {
+    try {
+        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        return next();
+    } catch (error) {
+        if (error.name === "TokenExpriedError") {
+            return res.status(419).json({
+                code: 419,
+                message: "토큰이 만료되었습니다.",
+            });
+        }
+        return res.status(401).json({
+            code: 401,
+            message: "유효하지 않는 토큰입니다.",
+        });
+    }
 };
